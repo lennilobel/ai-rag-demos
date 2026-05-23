1b9d7657ae47d921720b753718d7985e4b4f19bc
Leonard Lobel <lenni.lobel@sleektech.com>
2026-05-23 18:10:52 -0500

    Enable external REST endpoint

--- SqlDatabase/Rag.MoviesDatabase.SqlServer2025/Deployment/PreDeployment.sql
+++ SqlDatabase/Rag.MoviesDatabase.SqlServer2025/Deployment/PreDeployment.sql
@@ -1,14 +1,17 @@
-﻿IF NOT EXISTS (SELECT 1 FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
+﻿EXEC sp_configure 'show advanced options', 1
+RECONFIGURE
+GO
+
+EXEC sp_configure 'external rest endpoint enabled', 1
+RECONFIGURE
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
     CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Str0ngP@$$w0rd'
 GO
 
 IF NOT EXISTS (SELECT 1 FROM sys.database_scoped_credentials WHERE name = N'https://lenni-openai.openai.azure.com')
     CREATE DATABASE SCOPED CREDENTIAL [https://lenni-openai.openai.azure.com]
-    WITH
-        IDENTITY = 'HTTPEndpointHeaders',
-        SECRET = '{"api-key":"$(AzureOpenAIKey)"}'
-ELSE
-    ALTER DATABASE SCOPED CREDENTIAL [https://lenni-openai.openai.azure.com]
     WITH
         IDENTITY = 'HTTPEndpointHeaders',
         SECRET = '{"api-key":"$(AzureOpenAIKey)"}'
