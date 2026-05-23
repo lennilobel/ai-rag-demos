150d25d969b48eafa83c0587dae392dd75fa4bb7
Leonard Lobel <lenni.lobel@sleektech.com>
2026-05-23 18:04:49 -0500

    Publish SQL2025

--- SqlDatabase/Rag.MoviesDatabase.SqlServer2025/AzureOpenAI/AzureOpenAICredential.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Str0ngP@$$w0rd'
-GO
-
-CREATE DATABASE SCOPED CREDENTIAL [https://lenni-openai.openai.azure.com] WITH
-	IDENTITY = 'HTTPEndpointHeaders',
-	SECRET = '{"api-key": "1e981882b329481ebe4b2bfa261f8dce"}'
-GO
---
+++ SqlDatabase/Rag.MoviesDatabase.SqlServer2025/Deployment/PreDeployment.sql
@@ -0,0 +1,31 @@
+﻿IF NOT EXISTS (SELECT 1 FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
+    CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Str0ngP@$$w0rd'
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.database_scoped_credentials WHERE name = N'https://lenni-openai.openai.azure.com')
+    CREATE DATABASE SCOPED CREDENTIAL [https://lenni-openai.openai.azure.com]
+    WITH
+        IDENTITY = 'HTTPEndpointHeaders',
+        SECRET = '{"api-key":"$(AzureOpenAIKey)"}'
+ELSE
+    ALTER DATABASE SCOPED CREDENTIAL [https://lenni-openai.openai.azure.com]
+    WITH
+        IDENTITY = 'HTTPEndpointHeaders',
+        SECRET = '{"api-key":"$(AzureOpenAIKey)"}'
+GO
+
+IF EXISTS (SELECT 1 FROM sys.external_models WHERE name = N'MoviesTextEmbeddingModel')
+    DROP EXTERNAL MODEL MoviesTextEmbeddingModel
+GO
+
+CREATE EXTERNAL MODEL MoviesTextEmbeddingModel
+AUTHORIZATION dbo
+WITH (
+      LOCATION = 'https://lenni-openai.openai.azure.com/openai/deployments/lenni-text-embedding-3-large/embeddings?api-version=2023-03-15-preview',
+      API_FORMAT = 'Azure OpenAI',
+      MODEL_TYPE = EMBEDDINGS,
+      MODEL = 'lenni-text-embedding-3-large',
+      CREDENTIAL = [https://lenni-openai.openai.azure.com],
+      PARAMETERS = '{"dimensions" : 1536}'
+)
+GO
